4912b01a3bc72675d5da9d355afdb638789cf8a5
Habeebah Muse <omowumimuse@gmail.com>
2025-05-30 17:38:39 -0400

    did front end

---
+++ abiawomen/src/Component/MyDashboard/MyDashboard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function MyDashboard() {
+  return (
+    <div>
+        <h1>Welcome, Grace!<a></a></h1>
+        <div> 
+            <img/>
+            <div> 
+                <div> 
+                    <p>Membership Details</p>
+                    <p>Grace Iheme</p>
+                    <p>grace.iheme@gmail.com</p>
+                    <p>Status</p>
+                    <p>Verified Member</p>
+                    <p>ID Number</p>
+                    <p>AWA-MN0178</p>
+
+                </div>
+                <div> 
+                    <img></img>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+        <div> 
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
--- abiawomen/src/Component/MyDonation/MyDonation.jsx
+++ abiawomen/src/Component/MyDonation/MyDonation.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function MyDonation() {
   return (
-    <div>MyDonation</div>
+    <div>
+      <div>
+        <p>Donations History</p>
+        <p>Back</p>
+      </div>
+      <div> 
+        <img
+          src="https://picsum.photos/id/237/600/400"  // Specific image (black lab puppy)
+          alt="Black labrador puppy"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div> 
+        <select name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+        </select>
+        <button>Make a Donation</button>
+      </div>
+      <div>
+        <div> 
+          <h1>January 6 2023</h1>
+          <h1>Empowerment Program</h1>
+          <div>
+            <h1>NGN 10,500</h1>
+            <p>Completed</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
