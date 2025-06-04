0279167efc4a7e0148700acfa375aebefe9fe360
Habeebah Muse <omowumimuse@gmail.com>
2025-06-04 13:09:22 -0400

    dashboard

--- abiawomen/src/Component/MyDashboard/MyDashboard.jsx
+++ abiawomen/src/Component/MyDashboard/MyDashboard.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-
 export default function MyDashboard() {
   return (
     <div className='mx-10 my-10'>
         <h1 className='font-bold text-2xl my-10'>Welcome, Grace!<a></a></h1>
-        <div className='flex flex-row gap-10 justify-center'> 
-            <img  className="rounded-full border-4 w-40 h-40 items-center justify-center"src="https://picsum.photos/400/300" alt="Random placeholder image" />
-            <div className='flex flex-row'>
-                <div className='my-10 border p-3'> 
+        <div className='flex flex-row gap-10  justify-center'> 
+            <div className='flex items-center justify-center'>
+                <img className="rounded-full border-4 w-40 h-40" src="https://picsum.photos/400/300" alt="Random placeholder image" />
+            </div>
+            <div className='flex flex-row items-start border px-6 py-0'>
+                <div className='my-10 p-3'> 
                     <p>Membership Details</p>
                     <p>Grace Iheme</p>
                     <p>grace.iheme@gmail.com</p>
@@ -16,9 +16,24 @@ export default function MyDashboard() {
                     <p>ID Number</p>
                     <p>AWA-MN0178</p>
                 </div>
-                <div>
-                    <img/>
-                    <p>Holder</p>
+                <div className=" border flex justify-start flex-col py-3 px-3 my-3">
+                    <div className="flex justify-center">
+                        <img 
+                        src="https://picsum.photos/200/200" 
+                        alt="Profile placeholder"
+                        className="w-16 h-16"
+                        />
+
+                    </div>
+                    <hr className="border-t border-gray-300 my-4" />
+                    <h2 className="font-bold">MEMBERSHIP ID</h2>
+                    <div className="flex justify-center items-center"> {/* Centering container */}
+                    <img 
+                        src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=MEMBERSHIP-ID-AWA-MN0178" 
+                        alt="Membership QR Code"
+                        className="w-16 h-16 borde p-1"
+                    />
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,6 +50,7 @@ export default function MyDashboard() {
         <div> 
         </div>
         <div>
+            
             
         </div>
     </div>
