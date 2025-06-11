9958dc6a4c2c9946f781e9fe95fcf991ada55a8e
Habeebah Muse <omowumimuse@gmail.com>
2025-06-11 17:50:35 -0400

    benefit program

--- abiawomen/src/Component/BenefitPrograms/BenefitPrograms.jsx
+++ abiawomen/src/Component/BenefitPrograms/BenefitPrograms.jsx
@@ -4,10 +4,12 @@ export default function BenefitPrograms() {
     const [bookmarked, setBookmarked] = useState(false); 
   return (
     <div className='m-4'>
-        <div className='border py-3'>
+        <div className='border py-3 m-3'>
             <div className='py-3 px-3'>
                 <div className='flex flex-row justify-between'>
-                    <p>Leadership</p>
+                    <p className='font-bold'>
+                    <span className='bg-yellow-300'>Leadership</span>
+                    </p>
                     <button 
                     onClick={() => setBookmarked(!bookmarked)}
                     style={{ background: 'none', border: 'none', cursor: 'pointer' }}
@@ -27,35 +29,69 @@ export default function BenefitPrograms() {
                 </div>
                 <h2 className='font-bold text-[20px]'>Mentorship Initiative</h2>
                 <p>Connects women with experienced women leaders for guidance</p>
-                <div className='bg-blue-50'>
-                    <p className='font-bold'>Eligible</p>
-                </div>
+                <p className='font-bold'>
+                    <span className='bg-blue-50'>Eligible</span>
+                </p>
             </div>
         </div>
-        <div>
-            <div>
-                <div>
-                    <p>Leadership</p>
-                   <img/>
+        <div className='border py-3 m-3'>
+            <div className='py-3 px-3'>
+                <div className='flex flex-row justify-between'>
+                    <p className='font-bold'>
+                    <span className='bg-yellow'>Health</span>
+                    </p>
+                    <button 
+                    onClick={() => setBookmarked(!bookmarked)}
+                    style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+                    >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill={bookmarked ? "currentColor" : "none"}
+                        stroke="currentColor"
+                        strokeWidth="2"
+                    >
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
+                    </svg>
+                    </button>
                 </div>
-                <h2>Mentorship Initiative</h2>
-                <p>Connects women with experienced women leaders for guidance</p>
-                <div>
-                    <p>Eligible</p>
-                </div>
+                <h2 className='font-bold text-[20px]'>Maternal Care Assistance</h2>
+                <p>Access to prenatal and postnatal healthcare services</p>
+                <p className='font-bold'>
+                    <span className='bg-blue-50'>Eligible</span>
+                </p>
             </div>
         </div>
-        <div>
-            <div>
-                <div>
-                    <p>Leadership</p>
-                   <img/>
+        <div className='border py-3 m-3'>
+            <div className='py-3 px-3'>
+                <div className='flex flex-row justify-between'>
+                    <p className='font-bold'>
+                    <span className='bg-yellow'>Education</span>
+                    </p>
+                    <button 
+                    onClick={() => setBookmarked(!bookmarked)}
+                    style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+                    >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill={bookmarked ? "currentColor" : "none"}
+                        stroke="currentColor"
+                        strokeWidth="2"
+                    >
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
+                    </svg>
+                    </button>
                 </div>
-                <h2>Mentorship Initiative</h2>
-                <p>Connects women with experienced women leaders for guidance</p>
-                <div>
-                    <p>Eligible</p>
-                </div>
+                <h2 className='font-bold text-[20px]'>Scholarship Fund</h2>
+                <p>Provides financial aid for higeher education pursuits</p>
+                <p className='font-bold'>
+                    <span className='bg-blue-50'>Eligible</span>
+                </p>
             </div>
         </div>
     </div>
