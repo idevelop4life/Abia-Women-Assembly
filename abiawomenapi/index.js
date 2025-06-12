631ccb71440cb4fd66aa3b9f8e3fb18a5db4ab6c
Habeebah Muse <omowumimuse@gmail.com>
2025-06-12 15:37:48 -0400

    working on backend

---
+++ abiawomenapi/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config({path: __dirname + "./env"})
+const PORT = process.env.PORT || 9000; 
+const express = require('express')
+const app = express(); 
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on the port ${PORT}`)
+})
