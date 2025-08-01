d89402d8c6a9b980eba240ed166ec73f5babd1ad
Habeebah Muse <omowumimuse@gmail.com>
2025-08-01 16:14:59 -0400

    routes

--- abiawomenapi/routes/jwtAuth.js
+++ abiawomenapi/routes/jwtAuth.js
@@ -302,17 +302,20 @@ router.get("/verify", authorization, async (req, res) => {
 
 
     const result = await pool.query(
-      `SELECT first_name, last_name, email, primary_phone, nationality, state_city, profile_picture 
+      `SELECT first_name, last_name, email, primary_phone, nationality, state_city, profile_picture, is_verified
        FROM members 
        WHERE id = $1`,
       [userId],
     );
+
+    console.error("User verification result:", result.rows);
 
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "User not found" });
     }
 
     const user = result.rows[0];
+    console.error("User data:", user);
 
     res.json({
       verified: true,
