c3bc605d6e035b4e8566f655bcb2a063c9899378
Habeebah Muse <omowumimuse@gmail.com>
2025-09-09 11:08:39 -0400

    events

--- abiawomen/vite.config.js
+++ abiawomen/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173, // optional — just sets dev port
-    host: true  // optional — lets you access dev server from network
-  }
+  plugins: [react()]
 })
